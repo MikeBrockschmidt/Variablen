d7bb179a84f3c32c43a8c774c59640ed1802281b
Mike Brockschmidt <info@mike-brockschmidt.de>
2025-02-18 11:28:02 +0100

    13boxes

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String dd = "!";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String S = "Heute";
+  String spa = "";
+}
